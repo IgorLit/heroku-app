e45556993eb994a4665806d00cd64c59b4a77f65
IgorLit <igorek96by@gmail.com>
2017-03-29 12:26:33 +0300

    change let/const to var

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-let app = express();
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5011));
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.end("Hello world");
 });
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
